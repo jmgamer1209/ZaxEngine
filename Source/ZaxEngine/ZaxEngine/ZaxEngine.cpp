6ceadf1f1352209d0d8cc32cd1a81ed182e263c3
jiming <397207815@qq.com>
2023-12-12 23:50:14 +0800

    显示一个三角形，和UI界面同时存在

--- Source/ZaxEngine/ZaxEngine/ZaxEngine.cpp
+++ Source/ZaxEngine/ZaxEngine/ZaxEngine.cpp
@@ -1,39 +1,139 @@
 ﻿
 #include "ZaxEngine.h"
 
+void HandleRenderData();
+void DoRender();
+
+unsigned int VAO;
+unsigned int shaderProgram;
+unsigned int VBO;
+int success;
+char infoLog[512];
+
 int wWinMain()
 {
 	GLFW_INIT;
 
 	if (ImGui_Init() == 1) return 1;
+	
+	// 准备三角形数据，以及着色器程序，VAO
+	HandleRenderData();
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// 设置视口与背景
 		int display_w, display_h;
 		glfwGetFramebufferSize(window, &display_w, &display_h);
 		glViewport(0, 0, display_w, display_h);
 		glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
-		// ImGui 开始绘制
-		ImGui_NewFrame();
+		// 渲染三角形
+		DoRender();
 
-		// ImGui 显示窗口
+		// 渲染 UI 界面
+		ImGui_NewFrame(); // ImGui 开始绘制
 		ImGui_ShowSimpleWindow();
 		ImGui_ShowDemoWindow();
 		ImGui_ShowAnotherWindow();
-
-		// ImGui 生成渲染数据
-		ImGui::Render();
-
-		// ImGui 执行绘制指令
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		ImGui::Render(); // ImGui 生成渲染数据
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); // ImGui 执行绘制指令
 
 		// 缓冲区交换，将缓冲区数据显示到窗口
 		glfwSwapBuffers(window);
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
+
 	CleanUp();
 
 	return 0;
+}
+
+
+
+void HandleRenderData()
+{
+	//*********
+	// 编译 shader，并生成 shader program
+	//*********
+	const char* vertexShaderSource = "#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	const char* fragmentShaderSource = "#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n\0";
+
+ 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	
+	//*********
+	// 准备三角形mesh数据，并将顶点属性配置绑定在VAO中
+	//*********
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+	
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//设置顶点属性的顺序
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// 将绑定操作清除
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void DoRender()
+{
+	glUseProgram(shaderProgram);
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
