1866bfcc56c7d252bcb4c7d8f4a46d83936e31ca
jiming <397207815@qq.com>
2026-03-30 11:52:32 +0800

    【Physics】注释调整

--- Source/ZaxEngine/Component/RigidBody.cpp
+++ Source/ZaxEngine/Component/RigidBody.cpp
@@ -53,11 +53,14 @@ namespace ZaxEngine::Component {
 		// 获取物理世界变换四元数
 		auto custom_quat = Math::JPHQuatFromEularAngleYXZ(rotation);
 
-		// boxCenter 转到 Jolt 坐标系（X、Z 取反），再用 Jolt 四元数旋转到物理世界空间
-		// 正常来说，要把一个点从一个空间转换到另一个空间，本来是求出变换矩阵(或者变换四元数)，对原位置变换，但是因为位置关系比较简单，可以直接取反即可，上面又求出了旋转四元数，这样组合就等同于变换矩阵
-		JPH::Vec3 localCenter(-collider->boxCenter.x, collider->boxCenter.y, -collider->boxCenter.z);
-        JPH::Vec3 physicsWorldCenter = custom_quat * localCenter;
+		// 1.将引擎局部坐标转为物理局部坐标，因为变换关系非常简单，直接对 x,z 取反即可
+		JPH::Vec3 physicsLocalCenter(-collider->boxCenter.x, collider->boxCenter.y, -collider->boxCenter.z);
 
+		// 2.再将物理局部坐标转为物理世界坐标
+		// 正常来说，要把一个点从模型空间转换到世界空间，本来是求出变换矩阵(或者变换四元数)，但是因为这个 center 是一个偏移量，所以没有位移和缩放的影响，只需要旋转运算即可
+        JPH::Vec3 physicsWorldCenter = custom_quat * physicsLocalCenter;
+
+		// 物理世界坐标 = 物理世界坐标 + 物理世界偏移，而物理世界坐标和引擎世界坐标，只是坐标系轴的方向不同，不存在位移和缩放，所以这里直接对 x,z 取反就是物理世界坐标
 		settings.mPosition = JPH::RVec3(
 			-postion.x + physicsWorldCenter.GetX(),
 			 postion.y + physicsWorldCenter.GetY(),
@@ -80,19 +83,20 @@ namespace ZaxEngine::Component {
 		if (body != nullptr)
 		{
 			auto transform = this->gameObject->GetComponent<Transform>();
-			auto newPos = body->GetPosition();
-			auto quat = body->GetRotation();
-			auto physicsEular = Math::JPHEularAngleYXZFormJPHQuat(quat);
+			auto newPhysicsPos = body->GetPosition();
+			auto physicsQuat = body->GetRotation();
+			auto physicsEular = Math::JPHEularAngleYXZFormJPHQuat(physicsQuat);
 
-			// boxCenter 转到 Jolt 坐标系，用 Jolt 四元数旋转后还原渲染坐标系位置
-			JPH::Vec3 localCenter(-collider->boxCenter.x, collider->boxCenter.y, -collider->boxCenter.z);
-			JPH::Vec3 physicsWorldCenter = quat * localCenter;
+			JPH::Vec3 physicsLocalCenter(-collider->boxCenter.x, collider->boxCenter.y, -collider->boxCenter.z);
+			JPH::Vec3 physicsWorldCenter = physicsQuat * physicsLocalCenter;
 
 			transform->position = Vector3(
-				-(newPos.GetX() - physicsWorldCenter.GetX()),
-                 newPos.GetY() - physicsWorldCenter.GetY(),
-                -(newPos.GetZ() - physicsWorldCenter.GetZ())
+				-(newPhysicsPos.GetX() - physicsWorldCenter.GetX()),
+				newPhysicsPos.GetY() - physicsWorldCenter.GetY(),
+                -(newPhysicsPos.GetZ() - physicsWorldCenter.GetZ())
 			);
+
+			// physicsEular 是物理世界按照 YXZ 方向的欧拉角，因为x，z 是相反轴，所以直接取反即是引擎世界欧拉角
 			transform->rotation = Vector3(
 				-physicsEular.x,
 				physicsEular.y,
