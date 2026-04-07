b789eb630cc5eb08307260f38e70455106bdd4c3
jiming <397207815@qq.com>
2026-04-07 11:25:46 +0800

    【Physics】微调

--- Source/ZaxEngine/Runtime/Component/RigidBody.cpp
+++ Source/ZaxEngine/Runtime/Component/RigidBody.cpp
@@ -50,15 +50,16 @@ namespace ZaxEngine::Component {
 		auto postion = t->position;
 		auto rotation = t->rotation;
 
-		// 获取物理世界变换四元数
-		auto custom_quat = Math::JPHQuatFromEularAngleYXZ(rotation);
+		// 获取物理世界变换四元数，注意的是，这个 quat 是从物理局部坐标转为物理世界坐标，所以下面才需要把引擎局部转为物理局部
+		auto physics_quat = Math::JPHQuatFromEularAngleYXZ(rotation);
 
-		// 1.将引擎局部坐标转为物理局部坐标，因为变换关系非常简单，直接对 x,z 取反即可
+		// 1.将引擎局部坐标转为物理局部坐标，因为变换关系非常简单，直接对 x,z 取反即可，其实是一个简单的绕y轴的旋转变换
+		// 这里的局部坐标转换的说法可能产生困扰，其实 center 是相对模型空间的偏移，而初始状态下模型空间和引擎空间是对齐的，center 实际上就是引擎空间下的偏移，这里操作等同于引擎空间转换为物理空间
 		JPH::Vec3 physicsLocalCenter(-collider->boxCenter.x, collider->boxCenter.y, -collider->boxCenter.z);
 
 		// 2.再将物理局部坐标转为物理世界坐标
 		// 正常来说，要把一个点从模型空间转换到世界空间，本来是求出变换矩阵(或者变换四元数)，但是因为这个 center 是一个偏移量，所以没有位移和缩放的影响，只需要旋转运算即可
-        JPH::Vec3 physicsWorldCenter = custom_quat * physicsLocalCenter;
+        JPH::Vec3 physicsWorldCenter = physics_quat * physicsLocalCenter;
 
 		// 物理世界坐标 = 物理世界坐标 + 物理世界偏移，而物理世界坐标和引擎世界坐标，只是坐标系轴的方向不同，不存在位移和缩放，所以这里直接对 x,z 取反就是物理世界坐标
 		settings.mPosition = JPH::RVec3(
@@ -66,7 +67,7 @@ namespace ZaxEngine::Component {
 			 postion.y + physicsWorldCenter.GetY(),
 			-postion.z + physicsWorldCenter.GetZ()
 		);
-		settings.mRotation = custom_quat;
+		settings.mRotation = physics_quat;
 		settings.mFriction = 0.8f;
 		settings.SetShape(collider->GetShape());
 		settings.mObjectLayer = Layers::MOVING;
